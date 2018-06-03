1ad59ce4b6e153070f189ba0fa72abef59554d04
hxsieg@gmail.com <hxsieg>
2018-06-03 10:49:58 +0200

    test2

---
+++ pipe.rb
@@ -0,0 +1,9 @@
+require 'open3'
+
+Open3.popen3("sleep 2; ls -l") do |stdout, stderr, status, thread|
+  while line=stderr.gets do
+    puts(line)
+    puts "-------"
+  end
+  puts "STDOUT: #{stdout}"
+end
